3dfd245d6096532db2d9c66348d7ea10850b489d
Jeryn Aldaron Lau <aldaronlau@gmail.com>
2026-08-05 22:30:50 -0500

    pages: Move in html gen code from xtask

--- pages/src/lib.rs
+++ pages/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::fmt::Write;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use hatmil::{
+    Tree,
+    css::{Prop, Rule, Sel},
+};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Return HTML tree for the UI.
+pub fn build_html() -> Tree {
+    // Set up boilerplate
+    let mut tree = Tree::new();
+    let mut html = tree.html();
+    let mut head = html.head();
+
+    head.meta().charset("utf-8");
+    head.meta()
+        .name("viewport")
+        .content("width=device-width, initial-scale=1");
+    head.title_el().cdata("MealVote").close();
+    head.base().href("");
+    head.link().rel("icon").href("icon.svg");
+
+    // Set up style
+    let mut css = String::new();
+
+    write!(
+        &mut css,
+        "{}",
+        Rule::new(Sel::tp("html"), Prop::new().background_color("#213141"))
+    )
+    .ok();
+
+    head.style_el().cdata(css).close();
+
+    // Set up visible page
+    let mut body = html.body();
+
+    // Wasm linking
+    body.script()
+        .r#type("module")
+        .cdata(r#"import init, {} from './ui.js';init("./ui_bg.wasm");"#);
+
+    let _main = body.main();
+
+    tree
 }
--- xtask/src/build_html.rs
+++ xtask/src/build_html.rs
@@ -1,46 +1,10 @@
-use std::{fmt::Write, fs};
+use std::fs;
 
 use clot::Opts;
-use hatmil::{
-    Tree,
-    css::{Prop, Rule, Sel},
-};
 
 pub fn build_html(_opts: &dyn Opts) {
-    // Set up boilerplate
-    let mut tree = Tree::new();
-    let mut html = tree.html();
-    let mut head = html.head();
+    let tree = pages::build_html();
 
-    head.meta().charset("utf-8");
-    head.meta()
-        .name("viewport")
-        .content("width=device-width, initial-scale=1");
-    head.title_el().cdata("MealVote").close();
-    head.base().href("");
-    head.link().rel("icon").href("icon.svg");
-
-    // Set up style
-    let mut css = String::new();
-
-    write!(
-        &mut css,
-        "{}",
-        Rule::new(Sel::tp("html"), Prop::new().background_color("#213141"))
-    )
-    .ok();
-
-    head.style_el().cdata(css).close();
-
-    // Set up visible page
-    let mut body = html.body();
-
-    // Wasm linking
-    body.script()
-        .r#type("module")
-        .cdata(r#"import init, {} from './ui.js';init("./ui_bg.wasm");"#);
-
-    let _main = body.main();
     let res = concat!(env!("CARGO_MANIFEST_DIR"), "/../res");
     let build = concat!(env!("CARGO_MANIFEST_DIR"), "/../build");
 
